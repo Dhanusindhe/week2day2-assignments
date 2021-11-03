e9d0cfe095b536996a9bf6ad40dfb95f297e8673
Dhanusindhe <91777119+Dhanusindhe@users.noreply.github.com>
2021-11-03 20:19:16 +0530

    Add files via upload

---
+++ Assignment4.java
@@ -0,0 +1,29 @@
+package assignmentsweek2day2;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Assignment4 {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://acme-test.uipath.com/login");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("kumar.testleaf@gmail.com");
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("leaf@12");
+		driver.findElement(By.xpath("//button[@class='btn btn-primary']")).click();
+		String title = driver.getTitle();
+		System.out.println("The title is " + title);
+		driver.findElement(By.xpath("//a[text()='Log Out']")).click();
+		Thread.sleep(2000);
+		driver.close();
+
+	}
+
+}
